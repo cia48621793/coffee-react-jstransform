52ac58c30d339121eacd852297a18cf4d189fd43
Steve Fan <cia48621793@gmail.com>
2017-04-15 11:31:11 +0800

    updated to one file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./src/js-syntax-transform');
+var visitors = require('react-tools/vendor/fbtransform/visitors')
+var transform = require('jstransform').transform
+
+module.exports = function (input, options) {
+  var visitorList
+  if (options == null) {
+    options = {es6: true, react: true}
+  }
+  visitorList = visitors.getVisitorsBySet(['react', 'harmony'])
+  return transform(visitorList, input, options).code
+}
